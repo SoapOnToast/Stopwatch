d8ec285f7a53a0026597e0c12bc998db075af37e
William Ames <william.ames9@gmail.com>
2024-04-10 21:05:08 +1000

    cleanup

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -7,7 +7,6 @@ public class Stopwatch {
         StopwatchFrame stopwatchFrame = new StopwatchFrame(WINDOW_SIZE);
         StopwatchGUI stopwatchGUI = new StopwatchGUI(WINDOW_SIZE);
 
-
         stopwatchFrame.add(stopwatchGUI.frontPanel);
         stopwatchFrame.add(stopwatchGUI.backPanel);
         stopwatchFrame.add(stopwatchGUI.backgroundPanel);
@@ -15,7 +14,7 @@ public class Stopwatch {
         stopwatchFrame.setVisible(true);
 
         StopwatchTimer stopwatchTimer = new StopwatchTimer(stopwatchGUI.stopwatchLabel);
-        new StopwatchController( stopwatchFrame,stopwatchTimer);
+        new StopwatchController(stopwatchFrame,stopwatchTimer);
     }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Stopwatch::new);
--- src/StopwatchFrame.java
+++ src/StopwatchFrame.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class StopwatchFrame extends JFrame {
     private final String TITLE = "Stopwatch";
     private final String ICON_PATH = "/images/icon.png";
-    public StopwatchFrame(Dimension dimension) throws HeadlessException {
+    public StopwatchFrame(Dimension dimension){
         setLayout(null);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setSize(dimension.width, dimension.height);
@@ -13,6 +13,7 @@ public class StopwatchFrame extends JFrame {
         setLocation(getX(),0);
         setUndecorated(true);
         setAlwaysOnTop(true);
+
         Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource(ICON_PATH));
         setIconImage(icon);
         setTitle(TITLE);
