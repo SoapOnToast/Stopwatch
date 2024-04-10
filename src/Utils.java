0c686c97ebe9a9b62aadcbd9ab90b6b029c395e3
William Ames <William.ames9@gmail.com>
2024-04-10 21:59:21 +1000

    removed depreciated function

--- src/Utils.java
+++ src/Utils.java
@@ -1,9 +1,6 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Objects;
 
 public class Utils {
     public static Font loadCustomFont(String fontFileName, int size) {
@@ -16,12 +13,4 @@ public class Utils {
             return null;
         }
     }
-//    public static Image loadImage(String fileName) throws IOException {
-//        try{
-//            InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(fileName);
-//            return ImageIO.read(inputStream);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
