4914666d5b380a9d858d6b08534146c70de14d05
William Ames <William.ames9@gmail.com>
2024-04-10 22:07:01 +1000

    added comments

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -4,15 +4,16 @@ import java.awt.*;
 public class Stopwatch {
     private final Dimension WINDOW_SIZE = new Dimension(250,50);
     public Stopwatch() {
+        //setup frames
         StopwatchFrame stopwatchFrame = new StopwatchFrame(WINDOW_SIZE);
         StopwatchGUI stopwatchGUI = new StopwatchGUI(WINDOW_SIZE);
-
+        //add gui elements
         stopwatchFrame.add(stopwatchGUI.frontPanel);
         stopwatchFrame.add(stopwatchGUI.backPanel);
         stopwatchFrame.add(stopwatchGUI.backgroundPanel);
-
+        //enable frame
         stopwatchFrame.setVisible(true);
-
+        //link logic and controls
         StopwatchTimer stopwatchTimer = new StopwatchTimer(stopwatchGUI.stopwatchLabel);
         new StopwatchController(stopwatchFrame,stopwatchTimer);
     }
